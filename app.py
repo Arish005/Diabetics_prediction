b2d7755c2bd125e0914480fb3b038a172c6a4cd8
ARISH K S <143074862+Arish005@users.noreply.github.com>
2024-04-09 10:36:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,39 @@
-# Importing essential libraries
-from flask import Flask, render_template, request
+import streamlit as st
 import pickle
 import numpy as np
 
-# Load the Random Forest CLassifier model
+# Load the Random Forest Classifier model
 filename = 'diabetes-prediction-rfc-model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 
-app = Flask(__name__)
+# Function to predict diabetes
+def predict_diabetes(preg, glucose, bp, st, insulin, bmi, dpf, age):
+    data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
+    prediction = classifier.predict(data)
+    return prediction
 
-@app.route('/')
-def home():
-	return render_template('index.html')
+# Streamlit app
+def main():
+    # Page title
+    st.title("Diabetes Prediction")
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        preg = int(request.form['pregnancies'])
-        glucose = int(request.form['glucose'])
-        bp = int(request.form['bloodpressure'])
-        st = int(request.form['skinthickness'])
-        insulin = int(request.form['insulin'])
-        bmi = float(request.form['bmi'])
-        dpf = float(request.form['dpf'])
-        age = int(request.form['age'])
-        
-        data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
-        my_prediction = classifier.predict(data)
-        
-        return render_template('result.html', prediction=my_prediction)
+    # Input features
+    preg = st.slider("Pregnancies", 0, 17, 3)
+    glucose = st.slider("Glucose", 0, 199, 117)
+    bp = st.slider("Blood Pressure", 0, 122, 72)
+    sthickness = st.slider("Skin Thickness", 0, 99, 23)
+    insulin = st.slider("Insulin", 0, 846, 30)
+    bmi = st.slider("BMI", 0.0, 67.1, 32.0)
+    dpf = st.slider("DPF", 0.078, 2.42, 0.3725)
+    age = st.slider("Age", 21, 81, 29)
+
+    # Prediction
+    if st.button("Predict"):
+        result = predict_diabetes(preg, glucose, bp, sthickness, insulin, bmi, dpf, age)
+        if result[0] == 0:
+            st.success('The person is not diabetic')
+        else:
+            st.success('The person is diabetic')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    main()
